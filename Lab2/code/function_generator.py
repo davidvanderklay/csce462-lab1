8207c8a1ed85b62a9c3b5043e1fcdfc8278f0500
David Vanderklay <davidvanderklay@gmail.com>
2024-09-06 15:08:26 -0500

    makes the function generator use polling to avoid errors

--- Lab2/code/function_generator.py
+++ Lab2/code/function_generator.py
@@ -84,7 +84,12 @@ def generate_waveform(shape, frequency, max_voltage):
 
 def main():
     print("Press the button to start.")
-    GPIO.wait_for_edge(BUTTON_PIN, GPIO.RISING)
+    
+    # polling loop to wait for the button press
+    while GPIO.input(BUTTON_PIN) == GPIO.LOW:
+        time.sleep(0.01) # save cpu
+    
+    print("Button pressed, collecting user input.")
     shape, frequency, max_voltage = get_user_input()
     generate_waveform(shape, frequency, max_voltage)
 
