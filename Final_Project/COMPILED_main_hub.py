f3e0ae72dd7f276426bf51b9c9801821510577eb
Carlos Hernandez <hernandez_2002@tamu.edu>
2024-11-14 20:55:58 -0600

    moving arms in random directins

--- Final_Project/COMPILED_main_hub.py
+++ Final_Project/COMPILED_main_hub.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 import math
-from gpiozero import AngularServo
+import random
+from gpiozero import Servo
 from gpiozero.pins.pigpio import PiGPIOFactory
 
 # GPIO pin setup
@@ -10,8 +11,12 @@ MIC_PINS = [5, 6, 19]  # Input pins connected to the microphones
 
 # Servo setup
 factory = PiGPIOFactory()
-servo_pan = AngularServo(12, min_angle=0, max_angle=180, pin_factory=factory)
-servo_tilt = AngularServo(13, min_angle=0, max_angle=180, pin_factory=factory)
+factory2 = PiGPIOFactory()
+factory3 = PiGPIOFactory()
+
+servo = Servo(12, min_pulse_width=0.42/1000, max_pulse_width=2.35/1000, pin_factory=factory)
+servo2 = Servo(13, min_pulse_width=0.42/1000, max_pulse_width=2.35/1000, pin_factory=factory2)
+servo3 = Servo(26, min_pulse_width=0.42/1000, max_pulse_width=2.35/1000, pin_factory=factory3)
 
 # Constants
 SPEED_OF_SOUND = 343.0  # Speed of sound in m/s
@@ -34,6 +39,11 @@ def init_gpio():
 def cleanup_gpio():
     GPIO.cleanup()
 
+def randomMovement():
+    twoMotors = random.randint(-90, 90)
+    servo.value = math.sin(math.radians(random.randint(-90, 90)))
+    servo2.value = math.sin(math.radians(twoMotors))
+    servo3.value = math.sin(math.radians(twoMotors))
 
 def mic_callback(channel):
     global mic_times
@@ -65,8 +75,9 @@ def calculate_position():
     print(f"Calculated angle: {angle} degrees")
 
     # Move servos to point towards the object
-    servo_pan.angle = angle
-    servo_tilt.angle = 90  # Assuming horizontal movement only for simplicity
+    # servo_pan.angle = angle
+    # servo_tilt.angle = 90  # Assuming horizontal movement only for simplicity
+    randomMovement()
 
 
 def main():
